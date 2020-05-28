2556d0ad9f744336a247535ad1232644d5729d5b
gyxxpock <gyxxpock@gmail.com>
2020-05-28 10:07:54 -0500

    Changes: Optimized ToTitleCase method Added new unit test cases to cover all the cases Commented the routes for the API

--- src/Controllers/TodosController.cs
+++ src/Controllers/TodosController.cs
@@ -3,10 +3,17 @@ using System;
 using Microsoft.Extensions;
 using Exercise.Models;
 using System.Collections.Generic;
-using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
 
 namespace Exercise.Todos.Controllers
 {
+    // Routes get api/todos
+    // Routes get api/todos/:id
+    // Routes delete api/todo/:id
+    // Routes post api/todos
+    // Routes put api/todos/:id
     [Route("api/[controller]")]
     [ApiController]
     public class TodosController : ControllerBase
@@ -20,45 +27,55 @@ namespace Exercise.Todos.Controllers
             while ((line = Database.ReadLine()) != null)
             {
                 // Get line
-                ToDos.Add(new ToDo(line.Split(",")[0], line.Split(",")[1], line.Split(",")[2] == "true"));
+                ToDos.Add(new ToDo(line.Split(",")[0], ToTitleCase(line.Split(",")[1]), line.Split(",")[2] == "true"));
             }
 
             Database.Close();
-            var json = new JavaScriptSerializer().Serialize(ToDos);
-            return Ok();
+            
+            return Ok(JsonConvert.SerializeObject(ToDos, Formatting.Indented));
+        }
+        
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetTodo(int id)
+        {
+            return Ok(id);
         }
 
         public static string ToTitleCase(string todo)
         {
             var fs = todo.IndexOf(' ', 1);
-
-            if (todo[0] == ' ')
+            if(fs == -1)
             {
                 if (todo.Length > 1)
                 {
-                    if (fs != -1)
+                    if(todo[0] == ' ')
                     {
-                        return todo[0].ToString() + Char.ToUpper(todo[1]) + todo.Substring(2, fs - 2) + ToTitleCase(todo.Substring(fs));
-                    }
-                    else
+                        return String.Concat(todo[0], Char.ToUpper(todo[1]), todo.Substring(2));
+                    } else
                     {
-                        return todo[0].ToString() + Char.ToUpper(todo[1]) + todo.Substring(2);
+                        return String.Concat(Char.ToUpper(todo[0]), todo.Substring(1));
                     }
                 }
                 else
                 {
                     return todo.ToUpper();
                 }
-            }
-            else
+            } else
             {
-                if (fs == -1)
+                if(fs > 1)
                 {
-                    return Char.ToUpper(todo[0]) + todo.Substring(1);
-                }
-                else
+                    if (todo[0] == ' ')
+                    {
+                        return String.Concat(todo[0], Char.ToUpper(todo[1]), todo.Substring(2, fs-2)) + ToTitleCase(todo.Substring(fs));
+                    }
+                    else
+                    {
+                        return String.Concat(Char.ToUpper(todo[0]), todo[1..fs]) + ToTitleCase(todo.Substring(fs));
+                    }
+                } else
                 {
-                    return Char.ToUpper(todo[0]) + todo.Substring(1, fs - 1) + ToTitleCase(todo.Substring(fs));
+                    return " " + ToTitleCase(todo.Substring(fs));
                 }
             }
         }
--- tests/Controllers/TodosControllerTests.cs
+++ tests/Controllers/TodosControllerTests.cs
@@ -12,5 +12,26 @@ namespace Exercise.Todos.Controllers.Tests
 
             singleCharacterString.ShouldBe("H");
         }
+        [Fact]
+        public void Should_TitleCase_WhenStringIsMultipleCharacter()
+        {
+            var MultipleCharacterString = TodosController.ToTitleCase("hello world!");
+
+            MultipleCharacterString.ShouldBe("Hello World!");
+        }
+        [Fact]
+        public void Should_TitleCase_WhenStringStartsOrEndsWithSpaces()
+        {
+            var StartsOrEndsWithSpacesString = TodosController.ToTitleCase("   hello   world! ");
+
+            StartsOrEndsWithSpacesString.ShouldBe("   Hello   World! ");
+        }
+        [Fact]
+        public void Should_TitleCase_WhenStringContainsConsecutiveSpacesBetweenWords()
+        {
+            var ContainsConsecutiveSpacesBetweenWordsString = TodosController.ToTitleCase("hello    world!");
+
+            ContainsConsecutiveSpacesBetweenWordsString.ShouldBe("Hello    World!");
+        }
     }
 }

4d96584e65a18310f510c78f298c5e4b50a01769
Israel Salazar Chávez <israelch@mx1.ibm.com>
2020-05-27 23:19:06 -0500

    Updating
    .

---
+++ src/Models/ToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exercise.Models
+{
+    public class ToDo
+    {
+        public ToDo(string ID, string Description, Boolean IsDone)
+        {
+            this.ID = ID;
+            this.Description = Description;
+            this.IsDone = IsDone;
+        }
+
+        public string ID { get; set; }
+        public string Description { get; set; }
+        public Boolean IsDone { get; set; }
+    }
+}
